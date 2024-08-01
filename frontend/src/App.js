05e9c0abcb179f07606d7bdc2f143cf36163821b
Esme Ga <esgarrard95@gmail.com>
2024-08-01 17:45:22 +0100

    added to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const [message, setMessage] = useState('');
+
+const handleButtonClick = () => {
+  setMessage('Hello');
+}
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello World</h1>
+        {message && <p>{message}</p>}
+        <button onClick={handleButtonClick}>Click me</button>
       </header>
     </div>
   );
